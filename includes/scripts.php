538c1ab84e650a3d4aac04b8449d1055167a7f4f
Murtuza Mehdi <murtuzamehdi334@gmail.com>
2021-09-10 21:07:55 +0500

    udpsate

--- includes/scripts.php
+++ includes/scripts.php
@@ -29,6 +29,51 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TMM78JN');<\/script>
 <!-- End Google Tag Manager -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.13.0/dist/sweetalert2.all.min.js"></script>
+
+<?php 
+       	if(isset($_REQUEST['successMsg'])){
+          if ($_REQUEST['status']==1) {
+              echo "<script>Swal.fire({
+                  position: 'top-center',
+                  icon: 'success',
+                  title: '$_REQUEST[successMsg]'
+                })</script>";
+          } else {
+              echo "<script>Swal.fire({
+                  position: 'top-center',
+                  icon: 'warning',
+                  title: '$_REQUEST[successMsg]'
+                })</script>";
+          }
+          
+         
+      }
+      ?>
+      <script src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
+      <script>
+                
+          $(document).ready(function() {
+            $(".phone").keyup(function () {
+              $(this).attr("value", $(this).val());
+              if ($(this).val().length > 2) {
+                var twoNumbers = $(this).val().substr(0, 2);
+                if (twoNumbers == 33 || twoNumbers == 55 || twoNumbers == 81) {
+                  console.log("First: " + twoNumbers);
+                  $(".phone").inputmask({
+                    mask: "99-9999-9999"
+                  });
+                } else {
+                  console.log("Second: " + twoNumbers);
+                  $(".phone").inputmask({
+                    mask: "999-999-9999"
+                  });
+                }
+              }
+            });
+          });
+
+      </script>
   `);
 }, 10000);
 
@@ -104,29 +149,8 @@ $(document).on("click",".ys-popup-close.ys-exit",function(){
 
 function isNumberKey(t){var e=t.which?t.which:event.keyCode;return!(e>31&&(e<48||e>57))}
 </script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.13.0/dist/sweetalert2.all.min.js"></script>
 
-<?php 
-       	if(isset($_REQUEST['successMsg'])){
-          if ($_REQUEST['status']==1) {
-              echo "<script>Swal.fire({
-                  position: 'top-center',
-                  icon: 'success',
-                  title: '$_REQUEST[successMsg]'
-                })</script>";
-          } else {
-              echo "<script>Swal.fire({
-                  position: 'top-center',
-                  icon: 'warning',
-                  title: '$_REQUEST[successMsg]'
-                })</script>";
-          }
-          
-         
-      }
-      ?>
-      <script src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
-
+      
  <script>
 setTimeout(function(){  
     // $("#modalForDiscount").modal("show");
@@ -137,7 +161,7 @@ setTimeout(function(){
 
     });
     
-}, 2000);
+}, 10000);
 
 
 $(".fancybox").fancybox({
@@ -174,26 +198,6 @@ if(window.location.pathname=="/"){
 }
 
 
-
-$(document).ready(function() {
-  $(".phone").keyup(function () {
-    $(this).attr("value", $(this).val());
-    if ($(this).val().length > 2) {
-      var twoNumbers = $(this).val().substr(0, 2);
-      if (twoNumbers == 33 || twoNumbers == 55 || twoNumbers == 81) {
-        console.log("First: " + twoNumbers);
-        $(".phone").inputmask({
-          mask: "99-9999-9999"
-        });
-      } else {
-        console.log("Second: " + twoNumbers);
-        $(".phone").inputmask({
-          mask: "999-999-9999"
-        });
-      }
-    }
-  });
-});
 
 
 </script>
